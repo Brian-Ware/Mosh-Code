cc935ec541255e5c68f9cc1cb27f1cd2310d17d8
Brian Ware <gmontag25@gmail.com>
2021-12-08 11:25:55 -0500

    Rebrand

--- control-flow-exercises.js
+++ control-flow-exercises.js
@@ -1,3 +1,47 @@
+
+
+/**
+ * Hour
+ * If hour is between 6am and 12pm:Good Morning
+ * If it is between 12pm and 6pm: Good afternoon
+ * Otherwise: Good Evening 
+ */
+
+// let hour = 10;
+
+// if (hour >= 6 && hour < 12 ){
+//     console.log('Good Morning')
+// }
+// else if ( hour >= 12 && hour < 18){
+//     console.log('Good Afternoon')
+// }
+// else{
+//     console.log('Good Evening')
+// }
+
+// for(let i =1; i <= 5; i++){
+//     if(i % 2 !== 0 ) console.log(i)
+// }
+
+// let i = 0;
+// while (i <= 5){
+//     if(i % 2 !== 1 ) console.log(i)
+//     i++;
+// }
+
+// let i = 0;
+// do{
+//     if(i % 2 !== 1 ) console.log(i)
+//      i++;
+// } while(i <= 5);
+
+
+
+
+
+
+
+
 //Exercise 1 Max of Two Numbers:
 // function max(a,b){
 //  return (a > b) ? a : b;
@@ -37,7 +81,7 @@
 // Excercise 4 Demerit Points:
 function checkSpeed(speed) {
   const speedLimit = 70;
-  const kmPerPoint = 6;
+  const kmPerPoint = 7;
   if (speed < speedLimit + kmPerPoint) {
     console.log("Ok");
     return;
