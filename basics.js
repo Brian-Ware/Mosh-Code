4dda9a7ff00b8c0f99302055999f8f050df36dc6
Brian Ware <gmontag25@gmail.com>
2021-12-08 11:38:22 -0500

    Variables

---
+++ basics.js
@@ -0,0 +1,17 @@
+let name = 'Mosh';
+console.log(name);
+/** In the example abouve the actual variable is name. We use the let keyord to 
+ * declare a variable called name. After that we set it to the string Mosh. So basically
+ * Mosh is the data that goes inside the variable name. 
+ */
+/**Variable rules: 
+ * Cannot be a reserved keyword
+ * Must be camelCased
+ * Should be meaningful
+ * Cannot start with a number (1name)
+ * Cannot contain a space or hyphen (-)
+ * Are case-sensitive
+*/
+
+let firstName = 'Mosh';
+let lastName = 'Hamedani';
