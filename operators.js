baa84b9cd8bde731f94dd75ba161d3d2f1a9a6c7
Brian Ware <gmontag25@gmail.com>
2021-12-08 16:11:23 -0500

    Operators

---
+++ operators.js
@@ -0,0 +1,67 @@
+//Javascript Operators:
+/**Arithmetic
+ * Assignment
+ * Comparison
+ * Logical
+ * Bitwise
+ */
+
+//Arithmetic Operators:
+let x = 10;
+let y = 3;
+
+//Expressions
+  console.log(x + y);
+  console.log(x - y);
+  console.log( x * y);
+  console.log(x / y);
+  console.log(x % y);
+ console.log(x ** y);
+ 
+
+
+//  Increment (++)
+  console.log(x++);
+  console.log(x);
+  
+  //Decrement(--)
+  console.log(--x);
+ 
+
+//Assignment Operators
+
+let x = 10;
+/**
+ *x +=5 is the same as  x = x + 5;
+All the other expressions can be written with the += shorthand too.
+So x *= 3; is the same as x = x * 3; 
+ */
+
+//Comparison Operators
+let x = 1
+
+//Relational
+console.log(x > 0);
+console.log( x >= 1);
+console.log( x < 1);
+console.log( x <= 1);
+
+//Equality
+console.log(x === 1); //x is equal to 1
+console.log(x !== 1); // x is not equal to 1
+
+//Equality Operators
+
+// Its pretty simple see video if you forgot
+
+//Ternary Operator
+
+/** If a customer has more than 100 points,
+ * they are a 'gold' customer, otherwise, they
+ * are a 'silver' customer.
+ */
+
+let points = 110;
+let typeOfCustomer= points > 100? 'gold' : 'silver';
+
+console.log(typeOfCustomer);
